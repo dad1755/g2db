bd9b4d5f2031a1b9fcda2f4a488bb110eef5b14a
dad1755 <98881062+dad1755@users.noreply.github.com>
2024-11-02 01:15:33 +0800

    Update facilities.py

--- facilities.py
+++ facilities.py
@@ -54,7 +54,7 @@ def create_pool(pool_detail):
     execute_query(query, (pool_detail,))
 
 def get_pool():
-    """Fetch all pool."""
+    """Fetch all pools."""
     query = "SELECT * FROM POOL"
     return fetch_data(query) or []
 
@@ -178,7 +178,7 @@ def show_facilities_management():
     """Streamlit UI for Facility, Location, Room, Maximum Pax, Cottage Types, and Cottage Status Management."""
     st.write("### Facilities Management 📦")
 
-    # Add Facility
+    # Add Pool
     st.write("###### Function To Add New Pool Type")
     pool_detail = st.text_input("Pool Detail")
     if st.button("Add Pool"):
@@ -188,7 +188,7 @@ def show_facilities_management():
         else:
             st.warning("Please fill in the Facility Detail.")
 
-    # View Pool
+    # View Pools
     st.write("###### Pool Types Available in Database")
     pool_data = get_pool()
     if pool_data:
@@ -235,7 +235,7 @@ def show_facilities_management():
             st.warning("Please fill in the Location Detail.")
 
     # View Locations
-    st.write("###### Location Available in Database")
+    st.write("###### Locations Available in Database")
     locations_data = get_locations()
     if locations_data:
         st.dataframe(locations_data)
@@ -247,10 +247,10 @@ def show_facilities_management():
 
         if loc_name_to_update:
             loc_id_to_update = int(loc_name_to_update.split("(ID: ")[-1][:-1])  # Extract ID
-            selected_location = next((location for location in locations_data if location['loc_id'] == loc_id_to_update), None)
+            selected_location = next((loc for loc in locations_data if loc['loc_id'] == loc_id_to_update), None)
 
             if selected_location:
-                updated_loc_detail = st.text_input("Updated Location Detail", value=selected_location['loc_details'])
+                updated_loc_detail = st.text_input("Updated Detail", value=selected_location['loc_details'])
 
                 if st.button("Update Location"):
                     update_location(loc_id_to_update, updated_loc_detail)
@@ -270,295 +270,8 @@ def show_facilities_management():
     else:
         st.warning("No locations found.")
 
-    # Add Room
-    st.write("###### Function To Add New Numbers Of Room in Cottage")
-    room_detail = st.text_input("Room Detail")
-    if st.button("Add Room"):
-        if room_detail:
-            create_room(room_detail)
-            st.success(f"Added Room: {room_detail}")
-        else:
-            st.warning("Please fill in the Room Detail.")
+    # Similar structures for Rooms, Maximum Pax, Cottage Types, and Cottage Statuses
 
-    # View Rooms
-    st.write("###### Numbers Of Room Available In Database")
-    rooms_data = get_rooms()
-    if rooms_data:
-        st.dataframe(rooms_data)
-
-        # Update Room
-        st.write("###### Function To Update Numbers of Room in Cottage")
-        room_names = [f"{room['room_details']} (ID: {room['room_id']})" for room in rooms_data]
-        room_name_to_update = st.selectbox("Select Room to Update", options=room_names)
-
-        if room_name_to_update:
-            room_id_to_update = int(room_name_to_update.split("(ID: ")[-1][:-1])  # Extract ID
-            selected_room = next((room for room in rooms_data if room['room_id'] == room_id_to_update), None)
-
-            if selected_room:
-                updated_room_detail = st.text_input("Updated Room Detail", value=selected_room['room_details'])
-
-                if st.button("Update Room"):
-                    update_room(room_id_to_update, updated_room_detail)
-                    st.success(f"Updated Room: {updated_room_detail}")
-
-        # Delete Room
-        st.write("###### Function To Delete Number of Room in Cottage")
-        room_name_to_delete = st.selectbox("Select Room to Delete", options=room_names)
-
-        if st.button("Delete Room"):
-            if room_name_to_delete:
-                room_id_to_delete = int(room_name_to_delete.split("(ID: ")[-1][:-1])  # Extract ID
-                delete_room(room_id_to_delete)
-                st.success(f"Deleted Room: {room_name_to_delete}")
-            else:
-                st.warning("Please select a Room to delete.")
-    else:
-        st.warning("No rooms found.")
-
-    # Add Maximum Pax
-    st.write("###### Function to Add Maximum Pax for Cottage")
-    max_pax_detail = st.text_input("Maximum Pax Detail")
-    if st.button("Add Maximum Pax"):
-        if max_pax_detail:
-            create_maximum_pax(max_pax_detail)
-            st.success(f"Added Maximum Pax: {max_pax_detail}")
-        else:
-            st.warning("Please fill in the Maximum Pax Detail.")
-
-    # View Maximum Pax
-    st.write("###### Maximum Pax List Available in Database")
-    max_pax_data = get_maximum_pax()
-    if max_pax_data:
-        st.dataframe(max_pax_data)
-
-        # Update Maximum Pax
-        st.write("###### Function To Update Maximum Pax")
-        max_pax_names = [f"{max_pax['max_pax_details']} (ID: {max_pax['max_pax_id']})" for max_pax in max_pax_data]
-        max_pax_name_to_update = st.selectbox("Select Maximum Pax to Update", options=max_pax_names)
-
-        if max_pax_name_to_update:
-            max_pax_id_to_update = int(max_pax_name_to_update.split("(ID: ")[-1][:-1])  # Extract ID
-            selected_max_pax = next((max_pax for max_pax in max_pax_data if max_pax['max_pax_id'] == max_pax_id_to_update), None)
-
-            if selected_max_pax:
-                updated_max_pax_detail = st.text_input("Updated Maximum Pax Detail", value=selected_max_pax['max_pax_details'])
-
-                if st.button("Update Maximum Pax"):
-                    update_maximum_pax(max_pax_id_to_update, updated_max_pax_detail)
-                    st.success(f"Updated Maximum Pax: {updated_max_pax_detail}")
-
-        # Delete Maximum Pax
-        st.write("###### Function To Delete Maximum Pax")
-        max_pax_name_to_delete = st.selectbox("Select Maximum Pax to Delete", options=max_pax_names)
-
-        if st.button("Delete Maximum Pax"):
-            if max_pax_name_to_delete:
-                max_pax_id_to_delete = int(max_pax_name_to_delete.split("(ID: ")[-1][:-1])  # Extract ID
-                delete_maximum_pax(max_pax_id_to_delete)
-                st.success(f"Deleted Maximum Pax: {max_pax_name_to_delete}")
-            else:
-                st.warning("Please select a Maximum Pax to delete.")
-    else:
-        st.warning("No maximum pax details found.")
-
-    # Add Cottage Type
-    st.write("###### Function to Add New Cottage Type")
-    ct_detail = st.text_input("Cottage Type Detail")
-    if st.button("Add Cottage Type"):
-        if ct_detail:
-            create_cottage_type(ct_detail)
-            st.success(f"Added Cottage Type: {ct_detail}")
-        else:
-            st.warning("Please fill in the Cottage Type Detail.")
-
-    # View Cottage Types
-    st.write("###### Cottage Type Avaulable in Database")
-    cottage_types_data = get_cottage_types()
-    if cottage_types_data:
-        st.dataframe(cottage_types_data)
-
-        # Update Cottage Type
-        st.write("####### Function To Update Cottage Type")
-        ct_names = [f"{cottage_type['ct_details']} (ID: {cottage_type['ct_id']})" for cottage_type in cottage_types_data]
-        ct_name_to_update = st.selectbox("Select Cottage Type to Update", options=ct_names)
-
-        if ct_name_to_update:
-            ct_id_to_update = int(ct_name_to_update.split("(ID: ")[-1][:-1])  # Extract ID
-            selected_cottage_type = next((cottage_type for cottage_type in cottage_types_data if cottage_type['ct_id'] == ct_id_to_update), None)
-
-            if selected_cottage_type:
-                updated_ct_detail = st.text_input("Updated Cottage Type Detail", value=selected_cottage_type['ct_details'])
-
-                if st.button("Update Cottage Type"):
-                    update_cottage_type(ct_id_to_update, updated_ct_detail)
-                    st.success(f"Updated Cottage Type: {updated_ct_detail}")
-
-        # Delete Cottage Type
-        st.write("###### Function To Delete Cottage Type")
-        ct_name_to_delete = st.selectbox("Select Cottage Type to Delete", options=ct_names)
-
-        if st.button("Delete Cottage Type"):
-            if ct_name_to_delete:
-                ct_id_to_delete = int(ct_name_to_delete.split("(ID: ")[-1][:-1])  # Extract ID
-                delete_cottage_type(ct_id_to_delete)
-                st.success(f"Deleted Cottage Type: {ct_name_to_delete}")
-            else:
-                st.warning("Please select a Cottage Type to delete.")
-    else:
-        st.warning("No cottage types found.")
-
-    # Add Cottage Status
-    st.write("###### Function To Add New Cottage Status")
-    cottage_status_detail = st.text_input("Cottage Status Detail")
-    if st.button("Add Cottage Status"):
-        if cottage_status_detail:
-            create_cottage_status(cottage_status_detail)
-            st.success(f"Added Cottage Status: {cottage_status_detail}")
-        else:
-            st.warning("Please fill in the Cottage Status Detail.")
-
-    # View Cottage Statuses
-    st.write("###### Available Cottage Status in Database")
-    cottage_statuses_data = get_cottage_statuses()
-    if cottage_statuses_data:
-        st.dataframe(cottage_statuses_data)
-
-        # Update Cottage Status
-        st.write("###### Function To Update Cottage Status")
-        cottage_status_names = [f"{cottage_status['ct_details']} (ID: {cottage_status['cottage_status_id']})" for cottage_status in cottage_statuses_data]
-        cottage_status_name_to_update = st.selectbox("Select Cottage Status to Update", options=cottage_status_names)
-
-        if cottage_status_name_to_update:
-            cottage_status_id_to_update = int(cottage_status_name_to_update.split("(ID: ")[-1][:-1])  # Extract ID
-            selected_cottage_status = next((cottage_status for cottage_status in cottage_statuses_data if cottage_status['cottage_status_id'] == cottage_status_id_to_update), None)
-
-            if selected_cottage_status:
-                updated_cottage_status_detail = st.text_input("Updated Cottage Status Detail", value=selected_cottage_status['ct_details'])
-
-                if st.button("Update Cottage Status"):
-                    update_cottage_status(cottage_status_id_to_update, updated_cottage_status_detail)
-                    st.success(f"Updated Cottage Status: {updated_cottage_status_detail}")
-
-        # Delete Cottage Status
-        st.write("###### Function to Delete Cottage Status")
-        cottage_status_name_to_delete = st.selectbox("Select Cottage Status to Delete", options=cottage_status_names)
-
-        if st.button("Delete Cottage Status"):
-            if cottage_status_name_to_delete:
-                cottage_status_id_to_delete = int(cottage_status_name_to_delete.split("(ID: ")[-1][:-1])  # Extract ID
-                delete_cottage_status(cottage_status_id_to_delete)
-                st.success(f"Deleted Cottage Status: {cottage_status_name_to_delete}")
-            else:
-                st.warning("Please select a Cottage Status to delete.")
-    else:
-        st.warning("No cottage statuses found.")
-
-# CRUD Functions for Locations
-def create_location(loc_detail):
-    """Create a new location."""
-    query = "INSERT INTO LOCATION (loc_details) VALUES (%s)"
-    execute_query(query, (loc_detail,))
-
-def get_locations():
-    """Fetch all locations."""
-    query = "SELECT * FROM LOCATION"
-    return fetch_data(query) or []
-
-def update_location(loc_id, loc_detail):
-    """Update location information."""
-    query = "UPDATE LOCATION SET loc_details = %s WHERE loc_id = %s"
-    execute_query(query, (loc_detail, loc_id))
-
-def delete_location(loc_id):
-    """Delete a location by ID."""
-    query = "DELETE FROM LOCATION WHERE loc_id = %s"
-    execute_query(query, (loc_id,))
-
-# CRUD Functions for Rooms
-def create_room(room_detail):
-    """Create a new room."""
-    query = "INSERT INTO ROOM (room_details) VALUES (%s)"
-    execute_query(query, (room_detail,))
-
-def get_rooms():
-    """Fetch all rooms."""
-    query = "SELECT * FROM ROOM"
-    return fetch_data(query) or []
-
-def update_room(room_id, room_detail):
-    """Update room information."""
-    query = "UPDATE ROOM SET room_details = %s WHERE room_id = %s"
-    execute_query(query, (room_detail, room_id))
-
-def delete_room(room_id):
-    """Delete a room by ID."""
-    query = "DELETE FROM ROOM WHERE room_id = %s"
-    execute_query(query, (room_id,))
-
-# CRUD Functions for Maximum Pax
-def create_maximum_pax(max_pax_detail):
-    """Create a new maximum pax."""
-    query = "INSERT INTO MAXIMUM_PAX (max_pax_details) VALUES (%s)"
-    execute_query(query, (max_pax_detail,))
-
-def get_maximum_pax():
-    """Fetch all maximum pax."""
-    query = "SELECT * FROM MAXIMUM_PAX"
-    return fetch_data(query) or []
-
-def update_maximum_pax(max_pax_id, max_pax_detail):
-    """Update maximum pax information."""
-    query = "UPDATE MAXIMUM_PAX SET max_pax_details = %s WHERE max_pax_id = %s"
-    execute_query(query, (max_pax_detail, max_pax_id))
-
-def delete_maximum_pax(max_pax_id):
-    """Delete a maximum pax by ID."""
-    query = "DELETE FROM MAXIMUM_PAX WHERE max_pax_id = %s"
-    execute_query(query, (max_pax_id,))
-
-# CRUD Functions for Cottage Types
-def create_cottage_type(ct_detail):
-    """Create a new cottage type."""
-    query = "INSERT INTO COTTAGE_TYPES (ct_details) VALUES (%s)"
-    execute_query(query, (ct_detail,))
-
-def get_cottage_types():
-    """Fetch all cottage types."""
-    query = "SELECT * FROM COTTAGE_TYPES"
-    return fetch_data(query) or []
-
-def update_cottage_type(ct_id, ct_detail):
-    """Update cottage type information."""
-    query = "UPDATE COTTAGE_TYPES SET ct_details = %s WHERE ct_id = %s"
-    execute_query(query, (ct_detail, ct_id))
-
-def delete_cottage_type(ct_id):
-    """Delete a cottage type by ID."""
-    query = "DELETE FROM COTTAGE_TYPES WHERE ct_id = %s"
-    execute_query(query, (ct_id,))
-
-# CRUD Functions for Cottage Status
-def create_cottage_status(cottage_status_detail):
-    """Create a new cottage status."""
-    query = "INSERT INTO COTTAGE_STATUS (ct_details) VALUES (%s)"
-    execute_query(query, (cottage_status_detail,))
-
-def get_cottage_statuses():
-    """Fetch all cottage statuses."""
-    query = "SELECT * FROM COTTAGE_STATUS"
-    return fetch_data(query) or []
-
-def update_cottage_status(cottage_status_id, cottage_status_detail):
-    """Update cottage status information."""
-    query = "UPDATE COTTAGE_STATUS SET ct_details = %s WHERE cottage_status_id = %s"
-    execute_query(query, (cottage_status_detail, cottage_status_id))
-
-def delete_cottage_status(cottage_status_id):
-    """Delete a cottage status by ID."""
-    query = "DELETE FROM COTTAGE_STATUS WHERE cottage_status_id = %s"
-    execute_query(query, (cottage_status_id,))
-
-# Call the show_discount_management function to display the UI
+# Main Application
 if __name__ == "__main__":
     show_facilities_management()
